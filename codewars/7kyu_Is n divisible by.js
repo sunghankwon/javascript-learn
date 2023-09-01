e81511ec29cc136ff18e27a37b7b3f4a9a8d8a27
github sunghankwon <github akvo852@naver.com>
2023-09-01 13:40:12 +0900

    codewars 7kue

---
+++ codewars/7kyu_Is n divisible by.js
@@ -0,0 +1,31 @@
+/*
+Create a function that checks if the first argument n is divisible by all other arguments 
+(return true if no other arguments)
+
+Example:
+
+(6,1,3)--> true because 6 is divisible by 1 and 3
+(12,2)--> true because 12 is divisible by 2
+(100,5,4,10,25,20)--> true
+(12,7)--> false because 12 is not divisible by 7
+This kata is following kata: http://www.codewars.com/kata/is-n-divisible-by-x-and-y
+*/
+
+function isDivisible() {
+  //Write your code here
+  var answer = true;
+  var num = [];
+  for (let i = 0; i < arguments.length; i++) {
+    num.push(arguments[i]);
+  }
+  for (let i = 1; i < arguments.length; i++) {
+    if (num[0] % num[i] === 0) {
+      continue;
+    } else {
+      answer = false;
+      break;
+    }
+  }
+
+  return answer;
+}
---
+++ codewars/7kyu_Vowel Count.js
@@ -0,0 +1,25 @@
+/*
+Return the number (count) of vowels in the given string.
+
+We will consider a, e, i, o, u as vowels for this Kata (but not y).
+
+The input string will only consist of lower case letters and/or spaces.
+*/
+
+function getCount(str) {
+  var a = 0;
+  for (i = 0; i < str.length; i++) {
+    if (
+      str[i] === "a" ||
+      str[i] === "e" ||
+      str[i] === "i" ||
+      str[i] === "o" ||
+      str[i] === "u"
+    ) {
+      a++;
+    } else {
+      continue;
+    }
+  }
+  return a;
+}
